da38ada57d1b63f0190f1389779377ed6bb04b35
razieh zivaralam <razieh.zivaralam@gmail.com>
2025-01-22 13:10:25 +0100

    typo3 12 dosent support TYPO3_MODE and  TYPO3_MODE == 'BE' any more

--- Resources/PHP/Toolbar.php
+++ Resources/PHP/Toolbar.php
@@ -1,10 +1,12 @@
 <?php
 
-if (!defined('TYPO3_MODE')) {
+if (!defined('TYPO3')) {
 	die ('Access denied.');
 }
 
-if (TYPO3_MODE == 'BE') {
+use TYPO3\CMS\Core\Http\ApplicationType;
+
+if (ApplicationType::fromRequest(@$GLOBALS['TYPO3_REQUEST'])->isBackend()) {
 	// register the class as toolbar item
 	$GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems']['cabag_loginas'] = \Cabag\CabagLoginas\Hook\ToolbarItemHook::class;
 }
